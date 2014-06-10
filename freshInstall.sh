12ff4e556916cb99eede65ad1f5b379f5759e12e
Cheng Lou <chenglou92@gmail.com>
2014-06-09 21:49:11 -0700

    Install script for symlinking

---
+++ freshInstall.sh
@@ -0,0 +1,3 @@
+ln -s "$(pwd)/JavaScript" "$(pwd)/../JavaScript"
+ln -s "$(pwd)/Colorcoder-theme/Tomorrow-Night (SL) (Colorcode).tmTheme" "$(pwd)/../Colorcoder/Tomorrow-Night (SL) (Colorcode).tmTheme"
+
