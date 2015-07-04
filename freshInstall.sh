8cb49fb0c5dad7abb411c15919cf8c2692b1411f
Cheng Lou <chenglou92@gmail.com>
2015-07-04 16:38:53 +0200

    update

--- freshInstall.sh
+++ freshInstall.sh
@@ -1,3 +1,1 @@
 ln -s "$(pwd)/JavaScript" "$(pwd)/../JavaScript"
-ln -s "$(pwd)/Colorcoder-theme/Tomorrow-Night (SL) (Colorcode).tmTheme" "$(pwd)/../Colorcoder/Tomorrow-Night (SL) (Colorcode).tmTheme"
-
